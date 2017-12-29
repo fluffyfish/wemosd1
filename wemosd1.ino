72fa858274b845fa9b14605a7f50bb5c3dc402e0
fluffyfish <fluffy.fish@gmail.com>
2017-12-29 13:41:53 +0800

    Add files via upload
    Added in deep sleep option for 15 min posting to thingiverse

--- wemosd1.ino
+++ wemosd1.ino
@@ -1,49 +1,40 @@
-/* DHT Shield - Simple
+
+/* DHT Shield - Deep Sleep
  *
- * Example testing sketch for various DHT humidity/temperature sensors
- * Written by ladyada, public domain
+ * Displays humidity, temperature and heat index, sleeps for 10 seconds and repeats
+ *
+ * Connections:
+ * D0 -- RST
+ *
+ * If you cant reprogram as the ESP is sleeping, disconnect D0 - RST and try again
  *
  * Depends on Adafruit DHT Arduino library
  * https://github.com/adafruit/DHT-sensor-library
  */
 
 #include "DHT.h"
-
-#define DHTPIN D4     // what pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-#include <ESP8266WiFi.h>
-String apiKey = "xxxxxxxxxxxxxxxxxx"; 
-const char* ssid = "TheBananaStand";
-const char* password = "xxxxxxxxxx";
-const char* server = "api.thingspeak.com";
-int numReadings;
-WiFiClient client;
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
+#define DHTPIN D4
+#define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
-float temparray[6];
-float humidityarray[6];
-float hicarray[6];
-float tempavg;
-float humidityavg;
-float hicavg;
-void setup() {
-  // initialize serial communication at 115200 bits per second:
-  Serial.begin(115200);
+#include <ESP8266WiFi.h>
+String apiKey = "xxxxxxxxxxxxxxx";  
+const char* ssid = "TheBananaStand";
+const char* password = "xxxxxxxxxxxxxxx";
+const char* server = "api.thingspeak.com";
 
+// sleep for this many seconds
+const int sleepSeconds = 15*60;
+
+float humidity, temperature, heatIndex;
+char str_humidity[10], str_temperature[10], str_heatIndex[10];
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n\nWake up");
+
+  // Connect D0 to RST to wake up
+  pinMode(D0, WAKEUP_PULLUP);
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -56,74 +47,38 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  Serial.println();
   delay(10);
-  Serial.println("DHTxx test!");
+  Serial.println("Initialize DHT sensor");
   dht.begin();
-  numReadings = 0;
-  pinMode(BUILTIN_LED, OUTPUT);
+  delay(2000);
+
+  Serial.println("Read DHT sensor");
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
+
+  // Convert the floats to strings and round to 2 decimal places
+  dtostrf(humidity, 1, 2, str_humidity);
+  dtostrf(temperature, 1, 2, str_temperature);
+  dtostrf(heatIndex, 1, 2, str_heatIndex);
+
+  Serial.printf("Humidity:    %s %%\nTemperature: %s *C\nHeat index:  %s *C\n", str_humidity, str_temperature, str_heatIndex);
+
+  if (client.connect(server,80)) {
+    fnSendToThingspeak(temperature,humidity,heatIndex);
+  }
+  
+  Serial.printf("Sleep for %d seconds\n\n", sleepSeconds);
+
+  // convert to microseconds
+  ESP.deepSleep(sleepSeconds * 1000000, WAKE_RF_DEFAULT);
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  //delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  // float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    digitalWrite(BUILTIN_LED, HIGH);
-    return; 
-  } else {
-    digitalWrite(BUILTIN_LED, LOW);
-  }
-
-  // Compute heat index in Fahrenheit (the default)
-  // float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  //Serial.print(f);
-  //Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  //Serial.print(hif);
-  //Serial.println(" *F");
-  temparray[numReadings] = t;
-  humidityarray[numReadings] = h;
-  hicarray[numReadings] = hic;
-   if (client.connect(server,80)) {
-     if (numReadings == 5) {  // average over the six readings
-      // writing the average every 6 readings ~1 minute to match the front gate sensor
-      tempavg = (temparray[0]+temparray[1]+temparray[2]+temparray[3]+temparray[4]+temparray[5])/6;
-      humidityavg = (humidityarray[0]+humidityarray[1]+humidityarray[2]+humidityarray[3]+humidityarray[4]+humidityarray[5])/6;
-      hicavg = (hicarray[0]+hicarray[1]+hicarray[2]+hicarray[3]+hicarray[4]+hicarray[5])/6;
-      fnSendToThingspeak(tempavg,humidityavg,hicavg);
-      numReadings = 0;
-     }
-  }
-  
-  client.stop();
-  
-  Serial.println("Waiting 60 secs");
-  // thingspeak needs at least a 15 sec delay between updates
-  // 20 seconds to be safe
-  delay(9500);  // 9.5 secs between readings + time required to read temperature and humidity
-  numReadings++;
-  // Serial.println(numReadings);
 }
 
 void fnSendToThingspeak(float t , float h , float hic) {
@@ -146,6 +101,5 @@ void fnSendToThingspeak(float t , float h , float hic) {
     client.print("\n\n");
     client.print(postStr);
     Serial.println("Sending data to Thingspeak");
-    numReadings = 0;
 }
 
